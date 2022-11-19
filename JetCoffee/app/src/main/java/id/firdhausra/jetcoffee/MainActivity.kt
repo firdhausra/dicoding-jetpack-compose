d2065e778d635e0b7464eb728e6071ec9a394311
firdhausra <firdhaus.r.a@alumni.pcr.ac.id>
2022-11-19 18:05:46 +0700

    Create JetCoffee project.

---
+++ JetCoffee/app/src/main/java/id/firdhausra/jetcoffee/MainActivity.kt
@@ -0,0 +1,33 @@
+package id.firdhausra.jetcoffee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import id.firdhausra.jetcoffee.ui.theme.JetCoffeeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetCoffeeTheme {
+                JetCoffeeApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun JetCoffeeApp() {
+
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun JetCoffeeAppPreview() {
+    JetCoffeeTheme {
+        JetCoffeeApp()
+    }
+}
