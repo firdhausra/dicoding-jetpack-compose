b77b142fdd177ecd9da4232cc21718dad29d2f89
firdhausra <firdhaus.r.a@alumni.pcr.ac.id>
2022-11-24 00:52:28 +0700

    Create StatelessTemperatureInput.

---
+++ TemperatureConverter/app/src/main/java/id/firdhausra/temperatureconverter/ui/components/StatelessTemperatureInput.kt
@@ -0,0 +1,36 @@
+package id.firdhausra.temperatureconverter.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import id.firdhausra.temperatureconverter.R
+
+@Composable
+fun StatelessTemperatureInput(
+    input: String,
+    output: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateless_converter),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onValueChange,
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+    }
+}
