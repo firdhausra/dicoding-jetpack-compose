5fe67b98142ab97ef94e63cbd6ffcf41b00030bd
firdhausra <firdhaus.r.a@alumni.pcr.ac.id>
2022-11-24 00:51:18 +0700

    Create StatefulTemperatureInput.

---
+++ TemperatureConverter/app/src/main/java/id/firdhausra/temperatureconverter/ui/components/StatefulTemperatureInput.kt
@@ -0,0 +1,43 @@
+package id.firdhausra.temperatureconverter.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import id.firdhausra.temperatureconverter.R
+import id.firdhausra.temperatureconverter.utils.convertToFahrenheit
+
+@Composable
+fun StatefulTemperatureInput(
+    modifier: Modifier = Modifier,
+) {
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateful_converter),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            },
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+    }
+}
