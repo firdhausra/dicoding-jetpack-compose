da21071a33b3ef5641c94b1d93db8b35702f6e4a
firdhausra <firdhaus.r.a@alumni.pcr.ac.id>
2022-12-23 19:35:37 +0700

    Implement HeroListItem.

--- JetHeroes/app/src/main/java/id/firdhausra/jetheroes/JetHeroesApp.kt
+++ JetHeroes/app/src/main/java/id/firdhausra/jetheroes/JetHeroesApp.kt
@@ -1,9 +1,14 @@
 package id.firdhausra.jetheroes
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import id.firdhausra.jetheroes.component.HeroListItem
+import id.firdhausra.jetheroes.model.HeroesData
 import id.firdhausra.jetheroes.ui.theme.JetHeroesTheme
 
 @Composable
@@ -11,6 +16,15 @@ fun JetHeroesApp(
     modifier: Modifier = Modifier,
 ) {
     Box(modifier = modifier) {
+        LazyColumn {
+            items(HeroesData.heroes, key = { it.id }) { hero ->
+                HeroListItem(
+                    name = hero.name,
+                    photoUrl = hero.photoUrl,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
     }
 }
 
