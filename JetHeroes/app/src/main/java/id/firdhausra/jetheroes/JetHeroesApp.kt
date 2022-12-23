edca59106cd3fe789cca91606f954aa692da771d
firdhausra <firdhaus.r.a@alumni.pcr.ac.id>
2022-12-23 20:15:12 +0700

    Implement ScrollToTopButton.

--- JetHeroes/app/src/main/java/id/firdhausra/jetheroes/JetHeroesApp.kt
+++ JetHeroes/app/src/main/java/id/firdhausra/jetheroes/JetHeroesApp.kt
@@ -1,22 +1,46 @@
 package id.firdhausra.jetheroes
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import id.firdhausra.jetheroes.component.HeroListItem
+import id.firdhausra.jetheroes.component.ScrollToTopButton
 import id.firdhausra.jetheroes.model.HeroesData
 import id.firdhausra.jetheroes.ui.theme.JetHeroesTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun JetHeroesApp(
     modifier: Modifier = Modifier,
 ) {
     Box(modifier = modifier) {
-        LazyColumn {
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+        val showButton: Boolean by remember {
+            derivedStateOf { listState.firstVisibleItemIndex > 0 }
+        }
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 80.dp)
+        ) {
             items(HeroesData.heroes, key = { it.id }) { hero ->
                 HeroListItem(
                     name = hero.name,
@@ -24,6 +48,22 @@ fun JetHeroesApp(
                     modifier = Modifier.fillMaxWidth()
                 )
             }
+        }
+        AnimatedVisibility(
+            visible = showButton,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically(),
+            modifier = Modifier
+                .padding(bottom = 30.dp, end = 30.dp)
+                .align(Alignment.BottomEnd)
+        ) {
+            ScrollToTopButton(
+                onClick = {
+                    scope.launch {
+                        listState.scrollToItem(index = 0)
+                    }
+                }
+            )
         }
     }
 }
