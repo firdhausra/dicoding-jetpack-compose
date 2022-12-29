0a9de7f6d3ea411ba7dfdbd8fc8aea6f009ff677
firdhausra <firdhaus.r.a@alumni.pcr.ac.id>
2022-12-29 14:03:06 +0700

    Add ViewModelFactory.

--- JetHeroes/app/src/main/java/id/firdhausra/jetheroes/viewmodel/JetHeroesViewModel.kt
+++ JetHeroes/app/src/main/java/id/firdhausra/jetheroes/viewmodel/JetHeroesViewModel.kt
@@ -1,16 +1,28 @@
 package id.firdhausra.jetheroes.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import id.firdhausra.jetheroes.data.HeroRepository
 import id.firdhausra.jetheroes.model.Hero
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class JetHeroesViewModel(private val repository: HeroRepository) : ViewModel() {
+class JetHeroesViewModel(repository: HeroRepository) : ViewModel() {
     private val _groupedHeroes = MutableStateFlow(
         repository.getHeroes()
             .sortedBy { it.name }
             .groupBy { it.name[0] }
     )
     val groupedHeroes: StateFlow<Map<Char, List<Hero>>> get() = _groupedHeroes
+}
+
+class ViewModelFactory(private val repository: HeroRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(JetHeroesViewModel::class.java)) {
+            return JetHeroesViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
 }
